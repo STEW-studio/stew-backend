1f343ff26446db3675855a991c5a167ed77b2ca0
sangeun <olsang1203@gmail.com>
2024-07-19 19:54:06 +0900

    first commit

---
+++ src/main/java/studio/stew/StewApplication.java
@@ -0,0 +1,13 @@
+package studio.stew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studio/stew/StewApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.stew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
