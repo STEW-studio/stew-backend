0ac39a4f90698dc872ca5b035fe2520a19c8c40a
sangeun <olsang1203@gmail.com>
2024-07-23 17:32:59 +0900

    fix : wish_tutor 삭제

--- src/main/java/studio/stew/repository/WishTutorRepository.java
+++
@@ -1,7 +0,0 @@
-package studio.stew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import studio.stew.domain.mapping.WishTutor;
-
-public interface WishTutorRepository extends JpaRepository<WishTutor, Long> {
-}
