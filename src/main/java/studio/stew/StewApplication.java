57b394e84b3c8f17aec664f999b2b42fa5403fc5
sangeun <olsang1203@gmail.com>
2024-07-27 23:39:30 +0900

    feat : @EnableJpaAuditing 추가

--- src/main/java/studio/stew/StewApplication.java
+++ src/main/java/studio/stew/StewApplication.java
@@ -2,8 +2,10 @@ package studio.stew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StewApplication {
 
 	public static void main(String[] args) {
